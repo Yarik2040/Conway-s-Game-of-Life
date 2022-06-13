82179da0cdf7c2804e5ab221ffdf12f0f0fdd823
Yarik2040 <74916632+Yarik2040@users.noreply.github.com>
2022-06-13 23:03:28 +0300

    start!

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("DEMOLISHER!!!")
+import pygame
+
+pygame.init()
+sc = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit(0)
+
+    sc.fill((0, 0, 0))
+
+    pygame.display.flip()
+    clock.tick()
