08afa0709b6d5d5ede27c6865311a5d06795cb95
Yarik2040 <74916632+Yarik2040@users.noreply.github.com>
2022-06-13 23:25:35 +0300

    Check

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 pygame.init()
+
 sc = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
@@ -11,5 +12,9 @@ while True:
 
     sc.fill((0, 0, 0))
 
+    for i in range(0, 1290, 10):
+        pygame.draw.line(sc, (125, 125, 125), (i, 0), (i, 720))
+    for i in range(0, 730, 10):
+        pygame.draw.line(sc, (125, 125, 125), (0, i), (1280, i))
     pygame.display.flip()
     clock.tick()
